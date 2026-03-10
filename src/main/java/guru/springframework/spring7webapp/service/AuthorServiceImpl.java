96a133ed92f5ced32a9d69ac47be159121dda345
Lívia Carvalho Pereira Buriti <liviacburiti@outlook.com>
2026-03-10 16:09:34 -0300

    feat: implement author service layer

--- src/main/java/guru/springframework/spring7webapp/service/AuthorServiceImpl.java
+++ src/main/java/guru/springframework/spring7webapp/service/AuthorServiceImpl.java
@@ -1,4 +1,20 @@
 package guru.springframework.spring7webapp.service;
 
-public class AuthorServiceImpl {
+import guru.springframework.spring7webapp.domain.Author;
+import guru.springframework.spring7webapp.repository.AuthorRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthorServiceImpl implements AuthorService {
+
+    public final AuthorRepository authorRepository;
+
+    public AuthorServiceImpl(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @Override
+    public Iterable<Author> findAll() {
+        return authorRepository.findAll();
+    }
 }
