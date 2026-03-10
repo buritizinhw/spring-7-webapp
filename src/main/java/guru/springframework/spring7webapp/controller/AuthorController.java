2658e6a2cc4d8520fe0345d5dcf88c3346629224
Lívia Carvalho Pereira Buriti <liviacburiti@outlook.com>
2026-03-10 16:10:06 -0300

    feat: implement controller layer for author

--- src/main/java/guru/springframework/spring7webapp/controller/AuthorController.java
+++ src/main/java/guru/springframework/spring7webapp/controller/AuthorController.java
@@ -1,9 +1,22 @@
-package guru.springframework.spring7webapp.controller
+package guru.springframework.spring7webapp.controller;
 
-import org.springframework.stereotype.Controller
+import guru.springframework.spring7webapp.repository.AuthorRepository;
+import guru.springframework.spring7webapp.service.AuthorService;
+import org.antlr.v4.runtime.tree.pattern.ParseTreePattern;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 class AuthorController {
 
+    public final AuthorService authorService;
 
+    AuthorController(AuthorService authorService) {this.authorService = authorService;}
+
+    @RequestMapping(path = "/authors")
+    public String getAuthors(Model model) {
+        model.addAttribute("authors", authorService.findAll());
+        return "authors";
+    }
 }
